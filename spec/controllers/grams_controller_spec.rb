7693fb257d9892fe5f18a97ab640695ca4efe606
GeorgeFourikis <you@example.cogit config --global user.email you@example.co>
2016-06-07 17:20:27 +0000

    Added tests for image uploading too

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -154,12 +154,15 @@ end
     end
 
     
-    it "should successfully create a new gram in our database" do
+  it "should successfully create a new gram in our database" do
       user = FactoryGirl.create(:user)
       sign_in user
 
 
-    post :create, gram: { message: 'Hello!'}
+    post :create, gram: {
+      message: 'Hello!',
+      picture: fixture_file_upload("/picture.png", 'image/png')
+    }
     expect(response).to redirect_to root_path
     
     gram = Gram.last
