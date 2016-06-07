1058efe448b32156f03e21fa7ecd7229e597f576
GeorgeFourikis <you@example.cogit config --global user.email you@example.co>
2016-06-07 19:15:01 +0000

    Added tests onj comments

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -56,7 +56,5 @@ end
     params.require(:gram).permit(:message, :picture)
   end
 
-  def render_not_found(status=:not_found)
-    render text: "#{status.to_s.titleize} :(", status: status
-  end
+
 end
